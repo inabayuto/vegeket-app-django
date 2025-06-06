17a8714cf4aad65452fb8ec6ef5cdb4d0477caf3
稲葉勇人 <74127544+inabayuto@users.noreply.github.com>
2025-06-06 11:34:38 +0900

    カート機能追加

---
+++ src/base/views/item_views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from base.models import Item
+
+class IndexListView(ListView):
+    model = Item
+    template_name = "pages/index.html"
+
+class ItemDetailView(DetailView):
+    model =  Item
+    template_name = "pages/item.html"
+
+
+
